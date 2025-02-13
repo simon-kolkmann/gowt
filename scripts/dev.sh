9dc695f818ccdb089e1b84b9c66101a033273643
Simon Kolkmann <mail@simon.kolkmann.dev>
2025-02-13 18:29:55 +0100

    chore: add script for live reload during development

---
+++ scripts/dev.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# This script runs the app with live reloading for development purposes.
+#
+# See also:
+#   - watchexec: https://github.com/watchexec/watchexec
+#   - discussion: https://github.com/charmbracelet/bubbletea/issues/150#issuecomment-2492038498
+watchexec -r -c --wrap-process session -e go -- "go run ."
