b75d68559ca21a8d7f1f5e814bf4256347c5f4d3
Simon Kolkmann <mail@simon.kolkmann.dev>
2025-02-13 19:17:19 +0100

    refactor: remove unused short help

--- bubbles/help/help.go
+++ bubbles/help/help.go
@@ -19,12 +19,6 @@ type keyMap struct {
 	ChangeLanguage key.Binding
 }
 
-// ShortHelp returns keybindings to be shown in the mini help view. It's part
-// of the key.Map interface.
-func (k keyMap) ShortHelp() []key.Binding {
-	return []key.Binding{k.Quit}
-}
-
 // FullHelp returns keybindings for the expanded help view. It's part of the
 // key.Map interface.
 func (k keyMap) FullHelp() [][]key.Binding {
