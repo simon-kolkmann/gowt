20af7e86f423d59fe6d1eabd7e5f6b0e70c45dd1
Simon Kolkmann <mail@simon.kolkmann.dev>
2025-02-19 00:18:38 +0100

    refactor: model receiver variable names

--- bubbles/table/table.go
+++ bubbles/table/table.go
@@ -54,11 +54,11 @@ func createTable() table.Model {
 	return t
 }
 
-func (c *Model) Init() tea.Cmd {
+func (m *Model) Init() tea.Cmd {
 	return nil
 }
 
-func (c *Model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
+func (m *Model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 	var cmd tea.Cmd
 	var cmds []tea.Cmd = make([]tea.Cmd, 0)
 
@@ -69,7 +69,7 @@ func (c *Model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 
 		case key.Matches(msg, util.Keys.Delete):
 			entries := make([]types.Entry, 0)
-			cursor := c.table.Cursor()
+			cursor := m.table.Cursor()
 			entryIndex := len(store.GetEntries()) - 1 - cursor
 
 			for i, entry := range store.GetEntries() {
@@ -77,7 +77,7 @@ func (c *Model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 					entries = append(entries, entry)
 				}
 			}
-			c.table.SetCursor(cursor - 1)
+			m.table.SetCursor(cursor - 1)
 			cmds = append(cmds, store.SetEntries(entries))
 
 		case key.Matches(msg, util.Keys.AltDelete):
@@ -85,38 +85,38 @@ func (c *Model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 		}
 
 	case util.TimeTickMsg, messages.ClockInMsg, messages.ClockOutMsg:
-		c.calculateTableRows()
+		m.calculateTableRows()
 
 	case store.StoreChangedMsg:
 		// TODO: more specific messages
 		// language change
-		c.table = createTable()
-		c.entries = store.GetEntries()
-		c.calculateTableRows()
+		m.table = createTable()
+		m.entries = store.GetEntries()
+		m.calculateTableRows()
 	}
 
-	c.table, cmd = c.table.Update(msg)
+	m.table, cmd = m.table.Update(msg)
 	cmds = append(cmds, cmd)
 
-	return c, tea.Batch(cmds...)
+	return m, tea.Batch(cmds...)
 }
 
-func (c *Model) View() string {
-	return c.table.View()
+func (m *Model) View() string {
+	return m.table.View()
 }
 
-func (c *Model) calculateTableRows() {
+func (m *Model) calculateTableRows() {
 	rows := make([]table.Row, 0)
 
 	totalWorkTime := time.Time{}
 
 	// calculate total work time (all entries)
-	for _, entry := range c.entries {
+	for _, entry := range m.entries {
 		totalWorkTime = totalWorkTime.Add(entry.Duration())
 	}
 
-	for i := len(c.entries) - 1; i >= 0; i-- {
-		entry := c.entries[i]
+	for i := len(m.entries) - 1; i >= 0; i-- {
+		entry := m.entries[i]
 
 		if entry.End.IsZero() {
 			rows = append(rows, table.Row{
@@ -138,13 +138,13 @@ func (c *Model) calculateTableRows() {
 
 	}
 
-	c.table.SetRows(rows)
+	m.table.SetRows(rows)
 
 	const MAX_ROWS = 5
 
 	if len(rows) > MAX_ROWS {
-		c.table.SetHeight(MAX_ROWS + 2)
+		m.table.SetHeight(MAX_ROWS + 2)
 	} else {
-		c.table.SetHeight(len(rows) + 2)
+		m.table.SetHeight(len(rows) + 2)
 	}
 }
