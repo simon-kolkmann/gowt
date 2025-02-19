9d4693b4ce3289584eac3910b986523ad1394480
Simon Lorenz <mail@simon-lorenz.dev>
2025-02-19 12:05:59 +0000

    chore: create compressed archives in build script

--- scripts/build.sh
+++ scripts/build.sh
@@ -5,8 +5,10 @@ rm -r build
 
 echo '[1/2] Building linux/amd64...'
 GOARCH=amd64 GOOS=linux go build -ldflags="-X main.version=$(git describe --always --tags --dirty)" -o ./build/gowt-linux-amd64 .
+tar -czvf ./build/gowt-linux-amd64.tar.gz ./build/gowt-linux-amd64
 
 echo '[2/2] Building windows/amd64...'
 GOARCH=amd64 GOOS=windows go build -ldflags="-X main.version=$(git describe --always --tags --dirty)" -o ./build/gowt-win-amd64 .
+tar -czvf ./build/gowt-win-amd64.tar.gz ./build/gowt-win-amd64
 
 echo 'Done!'
