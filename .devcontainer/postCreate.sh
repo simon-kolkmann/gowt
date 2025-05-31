1f26b89ee72cf35beaf3f21378fefef35b616d44
Simon Kolkmann <mail@simon.kolkmann.dev>
2025-05-31 08:46:11 +0000

    chore: make ssh work inside dev container

--- .devcontainer/postCreate.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-curl -L -o /tmp/watchexec.deb https://github.com/watchexec/watchexec/releases/download/v2.3.0/watchexec-2.3.0-x86_64-unknown-linux-gnu.deb
-apt install /tmp/watchexec.deb
